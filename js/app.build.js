03b28142bd8febc748d1a12e95d4acd1695c1e08
jaykobpc <watenga.jacob@gmail.com>
2020-04-16 14:33:05 +0300

    fixes

---
+++ js/app.build.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        'id': 1,
+        'profile': 'alex mulei',
+        'username': 'alex',
+        'subdata': 'the book master',
+        'twitter': 'https://twitter.com/alexmulei',
+        'instagram': 'https://instagram.com/alexmulei',
+        'facebook': 'https://facebook.com/alexmulei',
+        'reddit': 'https://reddit.com/',
+        'website': 'https://codist.000webhostapp.com/'
+    }
+});
