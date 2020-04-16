20e38d4948619379f0ff4689b6d20b63e085093f
jaykobpc <watenga.jacob@gmail.com>
2020-04-16 18:57:29 +0300

    build

--- js/app.build.js
+++ js/app.build.js
@@ -5,10 +5,27 @@ var app = new Vue({
         'profile': 'alex mulei',
         'username': 'alex',
         'subdata': 'the book master',
-        'twitter': 'https://twitter.com/alexmulei',
-        'instagram': 'https://instagram.com/alexmulei',
-        'facebook': 'https://facebook.com/alexmulei',
+        'twitter': 'https://twitter.com/',
+        'instagram': 'https://instagram.com/',
+        'facebook': 'https://facebook.com/',
         'reddit': 'https://reddit.com/',
-        'website': 'https://codist.000webhostapp.com/'
+        'website': 'https://codist.000webhostapp.com/',
+        user: 'alexmulei',
+    },
+    mounted() {
+        this.add_metrics()
+    },
+    methods: {
+        add_metrics() {
+            axios.post('https://notesfileserver.000webhostapp.com/trackerstats/App/mount', JSON.stringify({
+                user: this.user,
+            }))
+                .then(function (response) {
+                    console.log('@root/system file mounted');
+                })
+                .catch(function(error) {
+                    console.log('@root/system file not mounted')
+                })
+        }
     }
 });
